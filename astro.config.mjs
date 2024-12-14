915dc8396e445d2fe862ddcb0fa970bdf7e5d5ca
yelsino <yelsino321@gmail.com>
2024-12-14 10:55:30 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  output: "server",
+  adapter: cloudflare(),
 });
