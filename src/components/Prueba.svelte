3bc80b7cd36712deb653a3291bbc60bd6838149d
yelsino <yelsino321@gmail.com>
2024-12-14 13:02:48 -0500

    cambio

---
+++ src/components/Prueba.svelte
@@ -0,0 +1,12 @@
+<script>
+  import toast, { Toaster } from "svelte-french-toast";
+</script>
+
+<button
+  aria-label="click para hola"
+  on:click={() => {
+    toast.success("hola mundo");
+  }}
+>
+probar botob
+</button>
